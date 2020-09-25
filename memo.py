e4381e1fdff5276ee072beafa3ae4233a027a56c
0913hung <61821556+0913hung@users.noreply.github.com>
2020-09-25 16:51:48 +0800

    fix toggle full

--- memo.py
+++ memo.py
@@ -18,6 +18,7 @@ class Example(tk.Frame):
 		self.value_list=[]
 		self.size_list=[]
 		self.create()
+		self.overrideredirect=False
 		
 		
 	def create(self):
@@ -29,34 +30,41 @@ class Example(tk.Frame):
 
 	def newWindow(self,value):
 		window = tk.Toplevel(self, bg='yellow')
+		window.title('')
+		#window.geometry('100x70+0+0')
 		
-		width=window.winfo_reqwidth()
-		height=window.winfo_reqheight()
 		
 		#if width<300:
 		#	width=300
 		window.geometry(value['size'])
-		label = tk.Text(window, bg='yellow', font=('Arial', 14))
+		label = tk.Text(window, bg='yellow', font=('Arial', 14), width=20, height=5)
 		label.pack(side="top", fill="both", expand=True)
 		label.insert(tk.END,value['data'])
+		#label.bind("<B3-Motion>", lambda event,label=window:self.move_me(event,label))
+		#window.bind("<B3-Motion>",self.move_me)
+		#window.bind("<Button-3>", self.getlocation)
 		self.value_list.append(label)
 		self.win_list.append(window)
 		
 	
 	def toggle_fill(self):
+		value=self.overrideredirect
 		for i in self.win_list:
-			#print(i.geometry())
-			#print(i.overrideredirect())
-			if not i.overrideredirect():
-				i.overrideredirect(True)
-			else:
+			if value:
 				i.overrideredirect(False)
+				self.overrideredirect=False
+			else:
+				i.overrideredirect(True)
+				self.overrideredirect=True
 				
 	def save(self):		
 		value=[]
 		with open("memo_content.txt", "w") as data:
-			for i,j in zip(self.value_list,self.win_list):				
-				value.append({"data":i.get("1.0", "end"),"size":j.geometry()})
+			for i,j in zip(self.value_list,self.win_list):
+				try:
+					value.append({"data":i.get("1.0", "end"),"size":j.geometry()})
+				except:
+					pass
 			data.write(json.dumps(value))
 	
 	def newWin(self):
@@ -65,10 +73,29 @@ class Example(tk.Frame):
 	def close(self):
 		self.save()
 		root.destroy()
+	
+	def getlocation(self,event):
+		widget=event.widget
+		self.x=widget.master.winfo_x()
+		self.y=widget.master.winfo_y()
+		print('location:')
+		print(self.x,self.y)
 		
+	def move_me(self, event):
+		label=event.widget
+		x = event.x+self.x
+		y = event.y+self.y
+		location='+{}+{}'.format(x,y)
+		label.master.geometry(location)
+		#print(self.x,self.y)
+		print(location)
+		#print(label.master.geometry())
+
+			
 
 if __name__ == "__main__":
 	root = tk.Tk()
+	
 	root.geometry('170x40+1770+860')
 	root.attributes('-topmost', 'true')
 	root.overrideredirect(True)
