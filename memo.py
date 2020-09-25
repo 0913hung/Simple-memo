45db53a214150da0e6453145938baaf29eece041
0913hung <0913hung@gmail.com>
2020-09-26 00:56:58 +0800

    1.0.0 all function should work

--- memo.py
+++ memo.py
@@ -1,104 +1,165 @@
 import tkinter as tk
+from tkinter.ttk import *
+from tkinter import messagebox
 import json
+import os
+import getpass
+
 
 class Example(tk.Frame):
-	def __init__(self, root):
-		tk.Frame.__init__(self, root)
-		#tk.Frame.overrideredirect(True)
-		b1 = tk.Button(self,border=0,bg='cornflowerblue', font=('Arial', 10),fg='white', text="Close", command = self.close)
-		b1.place(x=110, y=5)
-		b2 = tk.Button(self,border=0,bg='cornflowerblue', font=('Arial', 10),fg='white', text="Full", command = self.toggle_fill)
-		b2.place(x=77, y=5)
-		b3 = tk.Button(self,border=0,bg='cornflowerblue', font=('Arial', 10),fg='white', text="Save", command = self.save)
-		b3.place(x=37, y=5)
-		b4 = tk.Button(self,border=0,bg='cornflowerblue', font=('Arial', 10),fg='white', text="New", command = self.newWin)
-		b4.place(x=0, y=5)
-		#self.count = 0
-		self.win_list=[]
-		self.value_list=[]
-		self.size_list=[]
-		self.create()
-		self.overrideredirect=False
-		
-		
-	def create(self):
-		with open("memo_content.txt", "r") as data:
-			data=json.load(data)
-			for i in data:
-				self.newWindow(i)
-				
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
 
-	def newWindow(self,value):
-		window = tk.Toplevel(self, bg='yellow')
-		window.title('')
-		#window.geometry('100x70+0+0')
-		
-		
-		#if width<300:
-		#	width=300
-		window.geometry(value['size'])
-		label = tk.Text(window, bg='yellow', font=('Arial', 14), width=20, height=5)
-		label.pack(side="top", fill="both", expand=True)
-		label.insert(tk.END,value['data'])
-		#label.bind("<B3-Motion>", lambda event,label=window:self.move_me(event,label))
-		#window.bind("<B3-Motion>",self.move_me)
-		#window.bind("<Button-3>", self.getlocation)
-		self.value_list.append(label)
-		self.win_list.append(window)
-		
-	
-	def toggle_fill(self):
-		value=self.overrideredirect
-		for i in self.win_list:
-			if value:
-				i.overrideredirect(False)
-				self.overrideredirect=False
-			else:
-				i.overrideredirect(True)
-				self.overrideredirect=True
-				
-	def save(self):		
-		value=[]
-		with open("memo_content.txt", "w") as data:
-			for i,j in zip(self.value_list,self.win_list):
-				try:
-					value.append({"data":i.get("1.0", "end"),"size":j.geometry()})
-				except:
-					pass
-			data.write(json.dumps(value))
-	
-	def newWin(self):
-		self.newWindow({'data':'','size':''})
-	
-	def close(self):
-		self.save()
-		root.destroy()
-	
-	def getlocation(self,event):
-		widget=event.widget
-		self.x=widget.master.winfo_x()
-		self.y=widget.master.winfo_y()
-		print('location:')
-		print(self.x,self.y)
-		
-	def move_me(self, event):
-		label=event.widget
-		x = event.x+self.x
-		y = event.y+self.y
-		location='+{}+{}'.format(x,y)
-		label.master.geometry(location)
-		#print(self.x,self.y)
-		print(location)
-		#print(label.master.geometry())
+        self.win_list = [] #contain a list of Toplevel object
+        self.memo_list = [] #contain a list of Label object
+        self.hidden = False
 
-			
+        self.overrideredirect = False
+        self._offsetx = 0
+        self._offsety = 0
+        self.counter = 0
+        self.create()
+
+    def create(self):
+        print(os.path.exists("memo_content"))
+        try:
+            with open("memo_content", "r") as data:            
+                data = json.load(data)
+                if not len(data)==0:
+                    for i in data:
+                        self.new_window(i)
+                else:
+                    raise Exception()
+        except:
+            if not os.path.exists("memo_content"):
+                with open("memo_content", "w"):pass
+            self.new_blank_window()
+
+    def new_window(self, value):
+        window = tk.Toplevel(self, bg='yellow')
+        window.title('Memo')
+        window.overrideredirect(True)
+        window.geometry(value['size'])
+
+        # outter frame, contain all component
+        mainFrame = tk.Frame(window, bg='yellow')
+        mainFrame.pack(fill="both", expand=True)
+
+        # bottom frame, contain all bottom
+        button_frame = tk.Frame(mainFrame, height=115)
+        button_frame.pack(side='bottom', expand=False)
+
+        memo = tk.Text(mainFrame, bg='yellow', border=0, font=('Arial', value['font_size']), width=1, height=1)
+        memo.pack(fill="both", expand=True)
+        memo.insert(tk.END, value['data'])
+        memo.bind("<B3-Motion>", lambda event, window=window: self.dragwin(event, window))
+
+        window.bind("<Enter>", lambda event, button_frame=button_frame: self.show(event, button_frame))
+        window.bind("<Leave>", lambda event, button_frame=button_frame, window=window: self.hide(event, button_frame, window))
+        window.bind("<Button-3>", self.clickwin)
+
+        close = tk.Button(button_frame, command=lambda window=window: self.delete(window))
+        new = tk.Button(button_frame, command=self.new_blank_window)
+        save = tk.Button(button_frame, command=self.save)
+        front_increse = tk.Button(button_frame, command=lambda memo=memo: self.font_changer(memo, 1))
+        front_decrest = tk.Button(button_frame, command=lambda memo=memo: self.font_changer(memo, -1))
+
+        close.configure(width=4, border=0, bg='cornflowerblue', font=('Arial', 8), fg='white', text="Del")
+        new.configure(width=4, border=0, bg='cornflowerblue', font=('Arial', 8), fg='white', text="New")
+        save.configure(width=4, border=0, bg='cornflowerblue', font=('Arial', 8), fg='white', text="Save")
+        front_increse.configure(width=2, border=0, bg='cornflowerblue', font=('Arial', 8), fg='white', text="+")
+        front_decrest.configure(width=2, border=0, bg='cornflowerblue', font=('Arial', 8), fg='white', text="-")
+
+        new.pack(side='left', padx=1)
+        save.pack(side='left', padx=1)
+        front_increse.pack(side='left', padx=1)
+        front_decrest.pack(side='left', padx=1)
+        close.pack(side='left', padx=1)
+
+        self.memo_list.append(memo)
+        self.win_list.append(window)
+
+        memo = tk.Label(button_frame, text="X")
+        save.pack(side='left', padx=1)
+
+        grip = tk.ttk.Sizegrip(window)
+        grip.place(relx=1.0, rely=1.0, anchor="se")
+        grip.lift(memo)
+        grip.bind("<B1-Motion>", lambda event, window=window: self.on_motion(event, window))
+
+    def delete(self, memo):
+        message = tk.messagebox.askquestion('Delete', 'Confirm delete', icon='warning')
+        if message == 'yes':
+            memo.destroy()
+            self.save()
+        else:
+            return
+
+    def font_changer(self, memo, value):
+        size = memo['font'].split(' ')
+        size = int(size[1]) + value
+        memo['font'] = ('Arial', size)
+
+    def on_motion(self, event, window):
+        x1 = window.winfo_pointerx()
+        y1 = window.winfo_pointery()
+        x0 = window.winfo_rootx()
+        y0 = window.winfo_rooty()
+        window.geometry("%sx%s" % ((x1 - x0), (y1 - y0)))
+        return
+
+    def show(self, event, button_frame):
+        button_frame.pack(side='bottom', expand=False)
+        self.hidden = False
+
+    def hide(self, event, button_frame, window):
+        self.hidden = True
+
+        def a(self, button_frame, window):
+            if self.hidden:
+                button_frame.forget()
+                window.attributes('-topmost', False)
+                self.hidden = True
+
+        window.after(100, lambda self=self, button_frame=button_frame, window=window: a(self, button_frame, window))
+
+    def save(self):
+        values = []
+        with open("memo_content", "w") as data:
+            for i, j in zip(self.memo_list, self.win_list):
+                value = i.get("1.0", "end")
+                if value and value != "\n":
+                    font_size = i['font'].split(' ')
+                    values.append({"data": i.get("1.0", "end"), "size": j.geometry(), "font_size": font_size[1]})
+            data.write(json.dumps(values))
+
+    def new_blank_window(self):
+        self.new_window({'data': '', 'size': '160x86+953+513', 'font_size': 14})
+
+    def close(self):
+        self.save()
+        root.destroy()
+
+    def dragwin(self, event, window):
+        x = self.winfo_pointerx() - self._offsetx
+        y = self.winfo_pointery() - self._offsety
+        window.geometry('+{x}+{y}'.format(x=x, y=y))
+
+    def clickwin(self, event):
+        self._offsetx = event.x
+        self._offsety = event.y
+
+USER_NAME = getpass.getuser()
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
 
 if __name__ == "__main__":
-	root = tk.Tk()
-	
-	root.geometry('170x40+1770+860')
-	root.attributes('-topmost', 'true')
-	root.overrideredirect(True)
-	Example(root).pack(side="top", fill="both", expand=True)
-	root.mainloop()
-
+    add_to_startup()
+    root = tk.Tk()
+    root.overrideredirect(True)
+    Example(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
